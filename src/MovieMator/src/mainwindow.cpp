e8cdb80682b82b53a9af484724cc0127c3f26699
gdbwin <gdb_1986@163.com>
2019-01-19 11:04:05 +0800

    freeback改为feedback

--- src/MovieMator/src/mainwindow.cpp
+++ src/MovieMator/src/mainwindow.cpp
@@ -1878,7 +1878,7 @@ void MainWindow::on_actionAbout_TVE_triggered()
                 "</small>"
                 "<small>"
                 "<p />"
-                "<p><a href=\"http://www.moviemator.net/develop/third-party.html\">Related Information</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"mailto:market@effectmatrix.com\">Freeback</a></p>"
+                "<p><a href=\"http://www.moviemator.net/develop/third-party.html\">Related Information</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"mailto:market@effectmatrix.com\">Feedback</a></p>"
                 "</small>"
                 ).arg(qApp->applicationVersion()).arg(g_homePage));
 #endif
@@ -1895,7 +1895,7 @@ void MainWindow::on_actionAbout_TVE_triggered()
                 "</small>"
                 "<small>"
                 "<p />"
-                "<p><a href=\"http://www.moviemator.net/develop/third-party.html\">Related Information</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"mailto:market@effectmatrix.com\">Freeback</a></p>"
+                "<p><a href=\"http://www.moviemator.net/develop/third-party.html\">Related Information</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"mailto:market@effectmatrix.com\">Feedback</a></p>"
                 "</small>"
                 ).arg(qApp->applicationVersion()).arg(g_homePage));
 #else
@@ -1909,7 +1909,7 @@ void MainWindow::on_actionAbout_TVE_triggered()
                 "</small>"
                 "<small>"
                 "<p />"
-                "<p><a href=\"http://www.moviemator.net/develop/third-party.html\">Related Information</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"mailto:market@effectmatrix.com\">Freeback</a></p>"
+                "<p><a href=\"http://www.moviemator.net/develop/third-party.html\">Related Information</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"mailto:market@effectmatrix.com\">Feedback</a></p>"
                 "</small>"
                 ).arg(qApp->applicationVersion()).arg(g_homePage));
 #endif
