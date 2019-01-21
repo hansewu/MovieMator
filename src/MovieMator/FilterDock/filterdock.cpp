52c04ae96fa5c3b0a3bd938dd822ad8221b0d59e
wyl1987527 <wyl1987527@163.com>
2019-01-21 19:56:58 +0800

    filterdock load ui

--- src/MovieMator/FilterDock/filterdock.cpp
+++ src/MovieMator/FilterDock/filterdock.cpp
@@ -1,23 +1,62 @@
 #include "filterdock.h"
 #include "ui_filterdock.h"
 #include <QQuickWidget>
-#include <QDir>
-#include <QString>
+#include <QQuickView>
+#include <QQmlContext>
 #include <filterdockinterface.h>
+#include <QDir>
+#include <QStandardPaths>
+#include <QtQml>
+
+QList<FilterItemInfo*> *filterInfoList = new QList<FilterItemInfo*>();
 
 FilterDock::FilterDock(MainInterface *main, QWidget *parent):
     QDockWidget(parent),
   ui(new Ui::filterdock)
 {
-//    ui->setupUi(this);
-    QQuickWidget *view = new QQuickWidget(this);
-    view->setSource(QUrl::fromLocalFile("myqmlfile.qml"));
-    view->show();
+
+    UpdateFilters();
+
+//    QQuickView *view = new QQuickView;
+//    FiltersInfo *data = new FiltersInfo(filterInfoList);
+//    view->rootContext()->setContextProperty("metadatamodel", data);
+//    view->setSource(QUrl::fromLocalFile(filterUIPath));
+//    QWidget *container = QWidget::createWindowContainer(view);
+//    ui->gridLayout->addWidget(container);
+
 }
 FilterDock::~FilterDock()
 {
 
 }
+int FilterDock::UpdateFilters()
+{
+    if(filterInfoList->size() <= 0){
+        return -1;
+    }else{
+        QString filterUIPath = qApp->applicationDirPath();
+
+        #if defined(Q_OS_UNIX) && !defined(Q_OS_MAC)
+            filterUIPath = qApp->applicationDirPath() + "/../share/moviemator/qml/views/filter/FiltersLoader.qml”;
+        #elif defined(Q_OS_MAC)
+            QDir dir = QStandardPaths::standardLocations(QStandardPaths::DataLocation).first();
+            filterUIPath = dir.absolutePath() + "/share/moviemator/qml/views/filter/FiltersLoader.qml";
+        #endif
+
+        ui->setupUi(this);
+        FiltersInfo *data = new FiltersInfo(filterInfoList);
+        qmlRegisterType<FilterItemInfo>("FilterItemInfo", 1, 0, "FilterItemInfo");
+        ui->quickWidget->rootContext()->setContextProperty("metadatamodel", data);
+        ui->quickWidget->setResizeMode(QQuickWidget::SizeRootObjectToView);
+        ui->quickWidget->setSource(QUrl::fromLocalFile(filterUIPath));
+    }
+    return 0;
+}
+//FilterItemInfo::FilterItemInfo(QObject *parent){}
+//FilterItemInfo::FilterItemInfo(QObject *parent)
+//{
+//}
+
 
 static FilterDock *instance = 0;
 //初始化模块
@@ -39,10 +78,19 @@ void FilterDock_destroyModule()
 
 int setFiltersInfo(Filter_Info * filterInfos, int nFilterCount)
 {
+    filterInfoList->clear();
+    for(int i=0;i<nFilterCount;i++){
+        FilterItemInfo *filterInfo = new FilterItemInfo();
+        filterInfo->setName(QString(filterInfos[i].name));
+        filterInfo->setFilterType(QString(filterInfos[i].type));
+        filterInfo->setImageSourcePath(QString(filterInfos[i].imageSourcePath));
+        filterInfoList->append(filterInfo);
+    }
+    instance->UpdateFilters();
     return 0;
 }
 
-int getCurrentSelectedFilterIndex()
+int getCurrentedFilterIndex()
 {
     return 0;
 }
--- src/MovieMator/FilterDock/filterdock.h
+++ src/MovieMator/FilterDock/filterdock.h
@@ -3,12 +3,13 @@
 #include <QDockWidget>
 #include <maininterface.h>
 #include "filterdock_global.h"
+#include "filterdockinterface.h"
 
 namespace Ui {
     class filterdock;
 }
 
-
+//extern QList<Filter_Info> *filterInfoList;
 
 class FILTERDOCKSHARED_EXPORT FilterDock: public QDockWidget
 {
@@ -18,10 +19,55 @@ public:
     explicit FilterDock(MainInterface *main=0, QWidget *parent = 0);
     ~FilterDock();
 
+    int UpdateFilters();
 
 private:
     Ui::filterdock *ui;
     MainInterface *m_mainWindow;
+};
+
+class FilterItemInfo : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString filterType READ filterType WRITE setFilterType)
+    Q_PROPERTY(QString name READ name WRITE setName)
+    Q_PROPERTY(QString imageSourcePath READ imageSourcePath WRITE setImageSourcePath)
+
+public:
+
+    explicit FilterItemInfo(QObject *parent = 0){};
+
+    Q_INVOKABLE QString filterType() const { return m_filterType; }
+    Q_INVOKABLE void setFilterType(const QString strType){m_filterType = strType;};
+    Q_INVOKABLE QString name() const { return m_name; }
+    Q_INVOKABLE void setName(const QString strName){m_name = strName;};
+    Q_INVOKABLE QString imageSourcePath() const { return m_imageSourcePath; }
+    Q_INVOKABLE void setImageSourcePath(const QString strImageSourcePath){m_imageSourcePath = strImageSourcePath;};
+
+private:
+    QString m_filterType;
+    QString m_name;
+    QString m_imageSourcePath;
+};
+
+class FiltersInfo: public QObject
+{
+    Q_OBJECT
+public:
+    FiltersInfo(QList<FilterItemInfo*>* inList){m_filterInfoList = inList;};
+    ~FiltersInfo(){};
+
+    QVariant data(const QModelIndex &index, int role) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role);
+    QHash<int, QByteArray> roleNames() const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+
+    Q_INVOKABLE int rowCount(){return m_filterInfoList->size();};
+    Q_INVOKABLE FilterItemInfo* get(int index) const {return m_filterInfoList->at(index);};
+
+private:
+    typedef QList<FilterItemInfo*> FiltersList;
+    FiltersList * m_filterInfoList;
 };
 
 #endif // FILTERDOCK_H
