effc168f61a551477176f03503c1303f842481b8
wyl1987527 <wyl1987527@163.com>
2019-01-26 11:27:07 +0800

    fix bug：keyframes not working at “size” and “rotate” filter

--- src/MovieMator/src/qmltypes/qmlfilter.cpp
+++ src/MovieMator/src/qmltypes/qmlfilter.cpp
@@ -64,8 +64,8 @@ QmlFilter::~QmlFilter()
 Mlt::Animation QmlFilter::getAnimation(const QString& name)
 {
     if (m_filter) {
-//        const char* propertyName = name.toUtf8().constData();
-        const char* propertyName = name.toStdString().c_str();
+        const char* propertyName = name.toUtf8().constData();
+//        const char* propertyName = name.toStdString().c_str();
         if (!m_filter->get_animation(propertyName)) {
             // Cause a string property to be interpreted as animated value.
             m_filter->anim_get_double(propertyName, 0, 0);
