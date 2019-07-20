d8cb30058cde9b73964955243d451f0afd29f938
WanYuanCN <ebthon@hotmail.com>
2019-07-20 10:51:07 +0800

    test

--- src/MovieMator/RecentDock/recentdock.cpp
+++ src/MovieMator/RecentDock/recentdock.cpp
@@ -424,7 +424,7 @@ void RecentDock::on_lineEdit_textChanged(const QString& search)
         }
         m_proxyArray[i]->setFilterFixedString(search);
     }
-    Q_ASSERT(m_currentListView);
+//    Q_ASSERT(m_currentListView);  // 不用
     if(m_currentListView)
     {
         m_currentListView->clearSelection();
