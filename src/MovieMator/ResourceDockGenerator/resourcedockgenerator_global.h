7f4622d1d22be940085ddcaf986d00c3edda3e2a
Dragon-s <15919917852@163.com>
2019-07-22 11:14:45 +0800

    Resourcedockgenerator add interface

--- src/MovieMator/ResourceDockGenerator/resourcedockgenerator_global.h
+++ src/MovieMator/ResourceDockGenerator/resourcedockgenerator_global.h
@@ -12,10 +12,21 @@
 #  define RESOURCEDOCKGENERATORSHARED_EXPORT Q_DECL_IMPORT
 #endif
 
+typedef struct {
+    char strName[256];
+    char strType[256];
+    char strImageSourcePath[256];
+    bool bVisible;
+} FilterInfo;
+
 RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createTextDock(MainInterface *pMainInterface);
 
-RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createFilterDock(MainInterface *pMainInterface);
-
 RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createStickerDock(MainInterface *pMainInterface);
+
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createRecentDock(MainInterface *pMainInterface);
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_addFilesTRecentDock(const QString &filtePath);
+
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createFilterDock(MainInterface *pMainInterface);
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_setFiltersInfo(QDockWidget *pFilterDock, FilterInfo *filtersInfo, int nFilterCount);
 
 #endif // RESOURCEDOCKGENERATOR_GLOBAL_H
