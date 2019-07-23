49a64431756366b79a316c2598e4bdf26b37bf50
Dragon-s <15919917852@163.com>
2019-07-23 09:10:53 +0800

    Modify the ResourceDockGenerator interface

--- src/MovieMator/ResourceDockGenerator/resourcedockgenerator_global.h
+++ src/MovieMator/ResourceDockGenerator/resourcedockgenerator_global.h
@@ -14,8 +14,9 @@
 
 typedef struct {
     char strName[256];
-    char strType[256];
-    char strImageSourcePath[256];
+    char strClassification[256];
+    char strThumbnailFilePath[256];
+    int  nIndexOfMetadataModel;//保存filter在metadataModel中索引
     bool bVisible;
 } FilterInfo;
 
@@ -24,9 +25,9 @@ RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createTextDock(MainInterface *
 RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createStickerDock(MainInterface *pMainInterface);
 
 RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createRecentDock(MainInterface *pMainInterface);
-RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_addFilesTRecentDock(const QString &filtePath);
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_addFileToRecentDock(const QString &strFilePath);
 
-RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createFilterDock(MainInterface *pMainInterface);
-RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_setFiltersInfo(QDockWidget *pFilterDock, FilterInfo *filtersInfo, int nFilterCount);
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_createFilterDock(MainInterface *pMainInterface, bool isVideo = true);
+RESOURCEDOCKGENERATORSHARED_EXPORT QDockWidget *g_setFiltersInfo(QDockWidget *pFilterDock, FilterInfo *pFiltersInfo, int nFilterCount);
 
 #endif // RESOURCEDOCKGENERATOR_GLOBAL_H
