3b800b9f9dd7ac8945f09660e66a5930188c4347
Dragon-s <15919917852@163.com>
2019-07-26 10:22:49 +0800

    Modify the code style

--- src/MovieMator/src/mainwindow.cpp
+++ src/MovieMator/src/mainwindow.cpp
@@ -623,7 +623,7 @@ MainWindow::MainWindow()
     addResourceDock(m_resourceTextDock, tr("Text"), QIcon(":/icons/light/32x32/text.png"), QIcon(":/icons/light/32x32/text-highlight.png"));
 
     m_resourceStickerDock = g_createStickerDock(&MainInterface::singleton());
-    addResourceDock(m_resourceStickerDock, tr("Sticker"), QIcon(":/icons/light/32x32/anim-stickers.png"), QIcon(":/icons/light/32x32/anim-stickers-highlight.png"));
+    addResourceDock(m_resourceStickerDock, tr("Stickers"), QIcon(":/icons/light/32x32/anim-stickers.png"), QIcon(":/icons/light/32x32/anim-stickers-highlight.png"));
 
     m_propertiesDock = new QDockWidget(tr("Properties"));//, this);
     m_propertiesDock->installEventFilter(this);
