8400b77f54f409247c8af0e6c56bda63274c9167
Dragon-s <15919917852@163.com>
2019-08-01 10:20:41 +0800

    Add a comment to BaseDockWidget  #633

--- src/MovieMator/ResourceDockGenerator/basedockwidget.h
+++ src/MovieMator/ResourceDockGenerator/basedockwidget.h
@@ -24,20 +24,39 @@ public:
     virtual void setupUi() final;
 
 protected:
+    //用于listview中的项目个数变化时，更新Dock的宽。此函数为重载基类QDockWidget的函数
     virtual void resizeEvent(QResizeEvent *pEvent);
+
+    //用于项dock顶部工具栏添加控件
     virtual void setupOtherUi();
+
+    //创建所有分类的数据
     virtual QMap<QString, BaseItemModel *> *createAllClassesItemModel() = 0;
+
+    //是否有分类
     virtual bool hasClass() = 0;
+
+    //添加资源到时间线
     virtual void addToTimeline(const QStandardItem *pItem) = 0;
+
+    //预览预览资源
     virtual void preview(const QStandardItem *pItem) = 0;
 
 protected slots:
+    //点击item右上角的添加按钮
     virtual void addItemToTimeline(const QModelIndex &index);
+
+    //单击item
     virtual void clickedItem(const QModelIndex &index);
+
+    //显示右键菜单
     virtual void showMenu(const QModelIndex &index);
+
+    //当切换分类时，跳转到对应分类的listview
     virtual void onClassComboBoxActivated(int nIndex);
 
 private slots:
+    //切换dock时，自适应调节dock的宽
     void onDockWidgetVisibilityChanged(bool bVisible);
 
 private:
